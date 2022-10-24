199243f6dc21bf77d6d3734f5fa46738237dd9b9
Pedro Galetti <pedrogaletti12@gmail.com>
2022-10-24 09:53:00 -0300

    change channels module

--- channels/main.go
+++ channels/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+/*
+Channels are the medium through which goroutines can communicate with each other.
+In simple terms, a channel is a pipe that allows a goroutine to either put or read the data.
+*/
+
 func FirstChannels(channel chan int) {
 	channel <- 1
 	channel <- 2
@@ -15,6 +20,10 @@ func SecondChannels(channel chan int) {
 }
 
 func main() {
+	var c chan int
+	fmt.Println(c)                         // nil
+	fmt.Printf("Type of channel: %T\n", c) // Type of channel: chan int
+
 	fChannel := make(chan int)
 	sChannel := make(chan int)
 
